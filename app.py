6043b93ea0176f96cd0b216ef53a8e501027fd7e
Deyved Antonio <deyved.antonio@gmail.com>
2026-07-15 16:21:21 -0300

    feat: add app.py.

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(
+        page_title='Painel Maratonista',
+        page_icon=':film_strip:',
+        layout='wide',
+    )
+    st.title(':film_strip:')
+    st.title('Painel Maratonista')
+
+
+if __name__ == '__main__':
+    main()
